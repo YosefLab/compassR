d13ff3c756665c637eecbaac95ae9723be7188d1
sequoia-tree <sequoia@berkeley.edu>
2020-02-13 08:07:35 -0800

    Update Cohen\'s d calculation.

--- R/cohens_d.R
+++ R/cohens_d.R
@@ -10,10 +10,13 @@
 #' 
 #' @noRd
 cohens_d <- function(group_A_values, group_B_values) {
-    group_A_mean <- mean(group_A_values)
-    group_B_mean <- mean(group_B_values)
-    group_A_variance <- var(group_A_values)
-    group_B_variance <- var(group_B_values)
-    cohen_d <- (group_A_mean - group_B_mean) / sqrt((group_A_variance + group_B_variance) / 2)
+    n_A <- length(group_A_values)
+    n_B <- length(group_B_values)
+    mu_A <- mean(group_A_values)
+    mu_B <- mean(group_B_values)
+    var_A <- var(group_A_values)
+    var_B <- var(group_B_values)
+    pooled_sd <- sqrt(((n_A - 1) * var_A + (n_B - 1) * var_B) / (n_A + n_B - 2))
+    cohen_d <- (mu_A - mu_B) / pooled_sd
     cohen_d
 }
