040e4ab2bb67ef367df1ff6b9cbbebe21adeccca
Sequoia <sequoia@berkeley.edu>
2020-03-12 11:52:17 -0700

    Change gene_expression_statistics into a tibble.

--- R/CompassData.R
+++ R/CompassData.R
@@ -17,7 +17,7 @@ CompassData <- R6::R6Class(
         #' @field metareaction_consistencies Each row is a metareaction and each column is a cell. metareaction_consistencies[i, j] is the consistency (or "compatibility") between metareaction i and cell j.
         metareaction_consistencies = NULL,
 
-        #' @field gene_expression_statistics Each column describes a cell's total expression, metabolic expression, and metabolic activity.
+        #' @field gene_expression_statistics Each row describes a cell in terms of its ID, total expression, metabolic expression, and metabolic activity.
         gene_expression_statistics = NULL,
 
         #' @field cell_metadata The cell metadata from cell_metadata.csv. In this example it's the Th17 cell data from the papers linked above.
@@ -32,7 +32,7 @@ CompassData <- R6::R6Class(
         #' @field reaction_metadata The reaction metadata from the metabolic model (RECON2, by default).
         reaction_metadata = NULL,
 
-        #' @field reaction_partitions Each row describes a reaction in terms of its ID, undirected ID, direction, and metareaction ID.
+        #' @field reaction_partitions Each row describes a reaction (in terms of its ID, undirected ID, and direction) and which metareaction (i.e. reaction group) it belongs to.
         reaction_partitions = NULL,
 
         #' @description
@@ -46,10 +46,10 @@ CompassData <- R6::R6Class(
             metabolite_metadata <- read_compass_metadata(settings$metabolite_metadata_path)
             reaction_metadata <- read_compass_metadata(settings$reaction_metadata_path)
             cell_metadata <- read_compass_metadata(settings$cell_metadata_path)
-            compass_reaction_scores <- read_compass_matrix(settings$compass_reaction_scores_path, "reaction_id", suppress_warnings = TRUE)
+            compass_scores <- read_compass_matrix(settings$compass_scores_path, "reaction_id", suppress_warnings = TRUE)
             linear_gene_expression_matrix <- read_compass_matrix(settings$linear_gene_expression_matrix_path, "gene")
             reaction_consistencies <- get_reaction_consistencies(
-                compass_reaction_scores,
+                compass_scores,
                 min_consistency = settings$min_reaction_consistency,
                 min_range = settings$min_reaction_range
             )
@@ -127,9 +127,9 @@ CompassData <- R6::R6Class(
                 indent(get_tabular_data_representation(
                     self$gene_expression_statistics,
                     "gene_expression_statistics",
-                    "data frame",
-                    "statistics",
-                    "cells"
+                    "tibble",
+                    "cells",
+                    "fields"
                 )),
                 indent(get_tabular_data_representation(
                     self$cell_metadata,
